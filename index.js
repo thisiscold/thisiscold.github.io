e143fce1b1bdfe7de2a3893cd8c59a6a971d7286
judgejudes <judylee2021@u.northwestern.edu>
2021-01-01 12:09:12 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+// import rough from 'roughjs';
+
+// const rc = canvas(document.getElementById('canvas'));
+// rc.rectangle(10, 10, 200, 200); // x, y, width, height
+
+// function display() {
+//     alert("Hello World!");
+//  }
