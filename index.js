b200ee64f9c5a34e8367f78b2dd4f1ef1dbae560
judgejudes <judylee2021@u.northwestern.edu>
2021-01-02 14:53:59 -0500

    added some links

--- index.js
+++
@@ -1,8 +0,0 @@
-// import rough from 'roughjs';
-
-// const rc = canvas(document.getElementById('canvas'));
-// rc.rectangle(10, 10, 200, 200); // x, y, width, height
-
-// function display() {
-//     alert("Hello World!");
-//  }
